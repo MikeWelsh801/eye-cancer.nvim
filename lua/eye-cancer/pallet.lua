e83b084287311b8429623be86dd4e3b3fe3b7149
MICHAEL WELSH <mikewelsh801@gmail.com>
2024-04-05 17:10:14 -0600

    Lightened up the pallet slightly for a better contrast. Not too light!!!

--- lua/eye-cancer/pallet.lua
+++ lua/eye-cancer/pallet.lua
@@ -1,9 +1,9 @@
 return {
   green = '#60a060',
   yellow = '#b9b990',
-  brown = '#806630',
+  brown = '#907640',
   white = '#c8c8c0',
   off_white = '#959580',
   grey = '#646563',
-  dark_green = '#457079',
+  dark_green = '#659099',
 }
